e49160ebeffe1de70a298af952cb4624931a2855
JIVitian <jivitian@gmail.com>
2021-10-03 21:40:33 -0300

    Probando el hook useState para asignarle un evento a un boton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let urls = [
+    "https://media2.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e47fihp6o2bzi2czd50dpy3ckvxbdi5jymm10u1qhgq&rid=giphy.gif&ct=g",
+    "https://media0.giphy.com/media/vO4ik3XWjkQ2A/giphy.gif?cid=ecf05e478duubef0m871m69i6jr7rs2mx2p3o14w9686tbsq&rid=giphy.gif&ct=g",
+  ];
+
+  let urls2 = [
+    "https://media0.giphy.com/media/vO4ik3XWjkQ2A/giphy.gif?cid=ecf05e478duubef0m871m69i6jr7rs2mx2p3o14w9686tbsq&rid=giphy.gif&ct=g",
+    "https://media2.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e47fihp6o2bzi2czd50dpy3ckvxbdi5jymm10u1qhgq&rid=giphy.gif&ct=g",
+  ];
+  const [gifs, setGifs] = useState(urls);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {gifs.map((gif) => (
+          <img src={gif} alt="" />
+        ))}
+      </section>
+      <button onClick={() => setGifs(urls2)}>Reverse gifs</button>
     </div>
   );
 }
