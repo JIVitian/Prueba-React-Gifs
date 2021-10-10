8a7ad18ac4a9b9a96f4ff337cdfdb4c84858c83a
JIVitian <jivitian@gmail.com>
2021-10-10 20:41:57 -0300

    Añadiendo un boton para cambiar la keyword de los gifs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import GifsList from "./components/GifsList";
 
 function App() {
+  const [keyword, setKeyword] = useState("panda");
   return (
     <div className="App">
       <section className="App-content">
-        <GifsList keyword={'Marado'} />
+        <button onClick={() => setKeyword("Perro")}>Change Keyword</button>
+        <GifsList keyword={keyword} />
       </section>
     </div>
   );
--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,7 +1,7 @@
 const apiKey = "dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa";
 
-export default function getGifs({ keyWord = "maradona" } = {}) {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyWord}&limit=10&offset=0&rating=g&lang=en`;
+export default function getGifs({ keyword = "maradona" } = {}) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
   return fetch(apiURL)
     .then((res) => res.json())
     .then((response) => {
